89b7e6c40ef12d0db7a1d0f6096096c99f6d63ce
unknown <miguel.g3255@gmail.com>
2024-12-06 16:28:26 -0500

    push to build

--- Assets/Scripts/Interactions/ChangeLevel.cs
+++ Assets/Scripts/Interactions/ChangeLevel.cs
@@ -5,10 +5,12 @@ using UnityEngine;
 
 public class ChangeLevel : MonoBehaviour
 {
-    // Start is called before the first frame update
-    private void OnCollisionEnter(Collision other) {
-        if(other.gameObject.CompareTag("Player")){
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Player"))
+        {
             SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
         }
+
     }
 }
